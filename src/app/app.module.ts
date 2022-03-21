3e33a197fc1d75e8c6a71b732ae289f7ea0a17ac
KelseyErickson <kerickson@gmx.com>
2022-03-21 09:49:02 -0600

    Import AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-
 import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard.component";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "./hero.service";
-import { DashboardComponent } from "./dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent,
   ],
-  bootstrap: [AppComponent],
   providers: [HeroService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
